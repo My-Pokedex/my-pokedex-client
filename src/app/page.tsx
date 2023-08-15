574d52373960ca357d364506677b2b0956b3e48f
nalsae <nalsae0207@naver.com>
2023-08-15 21:53:11 +0900

    ✨ Feat : 구현한 SearchInput 컴포넌트 임시  적용 (#14)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Logo from '@components/Logo';
 import NavBar from '@components/NavBar';
+import SearchInput from '@components/SearchInput';
 
 export default function Home() {
   return (
     <main>
       <Logo location="main" />
       <NavBar />홈 페이지입니다 : )
+      <SearchInput type="main" />
     </main>
   );
 }
