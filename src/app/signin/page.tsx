40d374dd73f3d8b32b07acc492b068f416e59f7b
nalsae <nalsae0207@naver.com>
2023-08-16 23:16:07 +0900

    ✨ Feat : 구현한 AuthButton 컴포넌트 임시 적용 (#21)

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,5 +1,12 @@
 import Pokedex from '@components/Pokedex';
+import AuthButton from '@components/AuthButton';
 
 export default function Signin() {
-  return <Pokedex className="mb-[5rem]"> </Pokedex>;
+  return (
+    <Pokedex className="mb-[5rem]">
+      <AuthButton type="kakao" />
+      <AuthButton type="naver" />
+      <AuthButton type="google" />
+    </Pokedex>
+  );
 }
