26facf89e036b052e6235627a8079d3c65dcdbe3
nalsae <nalsae0207@naver.com>
2023-08-17 19:25:36 +0900

    ✏️ Chore : 변경된 img 파일 이름에 따라 경로 수정 (#22)

--- src/app/signin/layout.tsx
+++ src/app/signin/layout.tsx
@@ -4,7 +4,7 @@ export default function SigninLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex flex-col items-center justify-center h-full bg-[url('/assets/img/default_bg.png')] bg-cover bg-center bg-no-repeat">
+    <div className="flex flex-col items-center justify-center h-full bg-[url('/assets/img/bg_default.png')] bg-cover bg-center bg-no-repeat">
       {children}
     </div>
   );
