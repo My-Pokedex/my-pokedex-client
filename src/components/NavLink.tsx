39d3e3a3d2cfc61e161ec49369777be12297eb3c
nalsae <nalsae0207@naver.com>
2023-08-17 23:14:52 +0900

    ♻️ Refactor : 불필요한 변수 제거 (#29)

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -5,15 +5,13 @@ import { NAV_LINK_CONTENTS } from '@/constants/contents';
 type Destination = 'signin' | 'signout' | 'searchResult' | 'compatibility';
 
 export default function NavLink({ destination }: { destination: Destination }) {
-  const url = NAV_LINK_URL[destination];
-  const contents = NAV_LINK_CONTENTS[destination];
   const textSize = destination === 'signout' ? 'text-base' : 'text-lg';
 
   return (
     <Link
-      href={url}
+      href={NAV_LINK_URL[destination]}
       className={`block w-[6rem] h-[3.25rem] leading-[2.75rem] border-4 rounded-[.625rem] bg-navy-10 font-bold ${textSize} text-white-10 text-center shadow-outer/rightdown`}>
-      {contents}
+      {NAV_LINK_CONTENTS[destination]}
     </Link>
   );
 }
