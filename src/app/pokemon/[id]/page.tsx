6d68625a3628c80be79f05c81ee03bab58411bef
nalsae <nalsae0207@naver.com>
2023-08-20 02:03:31 +0900

    ✨ Feat : 구현한 InfoTag 컴포넌트 임시 적용 (#49)

--- src/app/pokemon/[id]/page.tsx
+++ src/app/pokemon/[id]/page.tsx
@@ -1,3 +1,21 @@
+import InfoTag from '@components/InfoTag';
+import { PokemonStat } from '@/types/common';
+import { INFO_TAG_STAT_CONTENTS } from '@/constants/contents';
+import { convertInfoTagName } from '@/utils/convertInfoTagName';
+
 export default function Pokemon() {
-  return <p>포켓몬 상세 정보 페이지입니다!</p>;
+  const tagInfo = Object.keys(INFO_TAG_STAT_CONTENTS) as PokemonStat[];
+
+  return (
+    <>
+      <InfoTag usage="ability" type="grass">
+        심록
+      </InfoTag>
+      {tagInfo.map((stat) => (
+        <InfoTag key={stat} usage="stat" stat={stat}>
+          {convertInfoTagName('kr', stat)}
+        </InfoTag>
+      ))}
+    </>
+  );
 }
