cabc9182f2761dfedf1047776e2a336f79b1708f
nalsae <nalsae0207@naver.com>
2023-08-20 00:20:28 +0900

    ✨ Feat : 구현한 PokemonCards 컴포넌트 임시 적용 (#36)

--- src/app/searchresult/page.tsx
+++ src/app/searchresult/page.tsx
@@ -1,20 +1,20 @@
 import TypeFilters from '@components/TypeFilters';
 import Dropdown from '@components/Dropdown';
-import PokemonCard from '@components/PokemonCard';
+import PokemonCards from '@components/PokemonCards';
 import { processCardsInfo } from '@/utils/processCardsInfo';
 import pokemons from '@/mocks/pokemons.json';
 
 export default function SearchResult() {
   return (
     <>
-      <section className="flex flex-col space-y-4">
+      <section className="flex flex-col space-y-4 mx-5">
         <h2 className="sr-only">필터 선택</h2>
         <TypeFilters usage="search" lang="kr" />
         <Dropdown />
       </section>
-      <main className="relative">
-        <PokemonCard
-          pokemonInfo={processCardsInfo(pokemons.data.pokemonInfo, 'kr')[0]}
+      <main>
+        <PokemonCards
+          pokemonCardsInfo={processCardsInfo(pokemons.data.pokemonInfo, 'kr')}
           lang="kr"
         />
       </main>
