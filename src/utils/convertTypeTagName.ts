d7e22aa3f6b9bd7344570d78199091d15cfbffb1
nalsae <nalsae0207@naver.com>
2023-08-20 01:59:13 +0900

    ✏️ Chore : 유틸 함수 convetTagName의 이름 변경 (#49)
    - convertInfoTagName 때문에 convertTypeTagName으로 변경

--- src/utils/convertTypeTagName.ts
+++ src/utils/convertTypeTagName.ts
@@ -1,7 +1,7 @@
 import { TYPE_TAG_CONTENTS } from '@/constants/contents';
 import { Lang, PokemonType } from '@/types/common';
 
-export const convertTagName = (lang: Lang, type: PokemonType) =>
+export const convertTypeTagName = (lang: Lang, type: PokemonType) =>
   lang === 'kr'
     ? TYPE_TAG_CONTENTS[type]
     : type.charAt(0).toUpperCase() + type.slice(1);
