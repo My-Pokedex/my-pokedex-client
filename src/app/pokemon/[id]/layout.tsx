cb34a0317c32e73684a2c96b7acef428bc613ec8
nalsae <nalsae0207@naver.com>
2023-08-21 00:43:01 +0900

    ♻️ Refactor : 전체 페이지 레이아웃의 중복 스타일 제거 (#51)

--- src/app/pokemon/[id]/layout.tsx
+++ src/app/pokemon/[id]/layout.tsx
@@ -8,9 +8,7 @@ export default function PokemonLayout({
   return (
     <>
       <Header />
-      <div className="flex flex-col items-center h-full pt-[5rem] bg-[url('/assets/img/bg_default.png')] bg-cover bg-center bg-no-repeat">
-        {children}
-      </div>
+      <div className="container-page">{children}</div>
     </>
   );
 }
