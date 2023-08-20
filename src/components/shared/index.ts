63e428a4cc13f40bae16bb1140268ddf63ba3449
nalsae <nalsae0207@naver.com>
2023-08-20 21:52:01 +0900

    ♻️ Refactor : 공용 컴포넌트를 shared 폴더 하위로 이동 (#58)

---
+++ src/components/shared/index.ts
@@ -0,0 +1,29 @@
+import Header from './Header/Header';
+import CatchButton from './CatchButton';
+import CommonButton from './CommonButton';
+import InfoContainer from './InfoContainer';
+import InfoTag from './InfoTag';
+import Logo from './Logo';
+import Pokedex from './Pokedex';
+import PokemonImage from './PokemonImage';
+import PokemonTitle from './PokemonTitle';
+import SearchInput from './SearchInput';
+import TypeFilters from './TypeFilters';
+import TypeTag from './TypeTag';
+import TypeTagContainer from './TypeTagContainer';
+
+export {
+  Header,
+  CatchButton,
+  CommonButton,
+  InfoContainer,
+  InfoTag,
+  Logo,
+  Pokedex,
+  PokemonImage,
+  PokemonTitle,
+  SearchInput,
+  TypeFilters,
+  TypeTag,
+  TypeTagContainer,
+};
