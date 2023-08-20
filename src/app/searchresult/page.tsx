29bd69b8786c97507c149d909cda92a78f877890
nalsae <nalsae0207@naver.com>
2023-08-20 19:38:22 +0900

    🐛 Fix : SearchResult 페이지의 스타일링 오류 수정 (#50)

--- src/app/searchresult/page.tsx
+++ src/app/searchresult/page.tsx
@@ -5,18 +5,17 @@ import { processCardsInfo } from '@/utils/processCardsInfo';
 import pokemons from '@/mocks/pokemons.json';
 
 export default function SearchResult() {
+  const processed = processCardsInfo(pokemons.data.pokemonCardsInfo, 'kr');
+
   return (
     <>
-      <section className="flex flex-col space-y-4 mx-5">
+      <section className="flex flex-col gap-4 mx-5 mt-[3.75rem] mb-4">
         <h2 className="sr-only">필터 선택</h2>
         <TypeFilters usage="search" lang="kr" />
         <Dropdown />
       </section>
       <main>
-        <PokemonCards
-          pokemonCardsInfo={processCardsInfo(pokemons.data.pokemonInfo, 'kr')}
-          lang="kr"
-        />
+        <PokemonCards pokemonCardsInfo={processed} lang="kr" />
       </main>
     </>
   );
