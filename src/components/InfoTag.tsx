cbd38520a5095f94489f9904ae5ff2b95ee1f88d
nalsae <nalsae0207@naver.com>
2023-08-20 19:29:14 +0900

    ♻️ Refactor : InfoTag 컴포넌트의 구조 및 스타일링 수정 (#50)
    - 접근성을 고려하여 구조 변경
    - 배경 색이 간헐적으로 적용되던 문제 해결

--- src/components/InfoTag.tsx
+++ src/components/InfoTag.tsx
@@ -10,12 +10,25 @@ interface InfoTagProps extends DefaultProps {
 }
 
 export default function InfoTag({ usage, type, stat, children }: InfoTagProps) {
-  const tagBg = usage === 'ability' ? `bg-type-${type}` : `bg-stat-${stat}`;
+  const abilityBg = type && `bg-type-${type}`;
+  const statBg = stat && INFO_TAG_STAT_BG[stat];
 
   return (
-    <dt
-      className={`px-4 py-[.625rem] rounded-full text-white-10 font-bold whitespace-nowrap shadow-md ${tagBg}`}>
+    <h4
+      className={`w-max px-4 py-[.625rem] rounded-full text-white-10 font-bold whitespace-nowrap shadow-md ${
+        usage === 'ability' ? abilityBg : statBg
+      }`}>
       {children}
-    </dt>
+    </h4>
   );
 }
+
+const INFO_TAG_STAT_BG = {
+  hp: 'bg-stat-hp',
+  attack: 'bg-stat-attack',
+  defense: 'bg-stat-defense',
+  's-attack': 'bg-stat-s-attack',
+  's-defense': 'bg-stat-s-defense',
+  speed: 'bg-stat-speed',
+  total: 'bg-stat-total',
+};
