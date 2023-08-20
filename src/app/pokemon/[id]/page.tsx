0c916751529db46ad6988f7a0d6e4ee4758fd755
nalsae <nalsae0207@naver.com>
2023-08-20 19:38:40 +0900

    ✨ Feat : 구현한 PokemonInfo 컴포넌트 임시 적용 (#50)

--- src/app/pokemon/[id]/page.tsx
+++ src/app/pokemon/[id]/page.tsx
@@ -1,21 +1,21 @@
-import InfoTag from '@components/InfoTag';
-import { PokemonStat } from '@/types/common';
-import { INFO_TAG_STAT_CONTENTS } from '@/constants/contents';
-import { convertInfoTagName } from '@/utils/convertInfoTagName';
+import PokemonInfo from '@components/PokemonInfo';
+import pokemonDefault from '@/mocks/pokemonDefault.json';
+import pokemonException from '@/mocks/pokemonException.json';
+import { processDetailInfo } from '@/utils/processDetailInfo';
+import CatchButton from '@components/CatchButton';
 
 export default function Pokemon() {
-  const tagInfo = Object.keys(INFO_TAG_STAT_CONTENTS) as PokemonStat[];
+  const processed = processDetailInfo(pokemonDefault.data, 'kr');
 
   return (
-    <>
-      <InfoTag usage="ability" type="grass">
-        심록
-      </InfoTag>
-      {tagInfo.map((stat) => (
-        <InfoTag key={stat} usage="stat" stat={stat}>
-          {convertInfoTagName('kr', stat)}
-        </InfoTag>
-      ))}
-    </>
+    <main className="relative mx-5 mt-[3.75rem]">
+      <h2 className="sr-only">{`${processed.name}의 상세 정보`}</h2>
+      <PokemonInfo pokemonDetailInfo={processed} lang="kr" />
+      <CatchButton
+        size="large"
+        isCatched={false}
+        className="absolute top-6 right-6"
+      />
+    </main>
   );
 }
