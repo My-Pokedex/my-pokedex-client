c62c278f333b7c8eb3ce4e837b01aac9a854169f
nalsae <nalsae0207@naver.com>
2023-08-21 19:53:02 +0900

    ✨ Feat : Compatibility 페이지의 임시 metadata 추가 (#55)

--- src/app/compatibility/layout.tsx
+++ src/app/compatibility/layout.tsx
@@ -1,4 +1,10 @@
+import type { Metadata } from 'next';
 import { Header } from '@/components';
+
+export const metadata: Metadata = {
+  title: 'My Pokedex - 타입별 상성 보기',
+  description: '포켓몬 배틀 시의 타입별 상성을 정리한 페이지입니다.',
+};
 
 export default function CompatibilityLayout({
   children,
