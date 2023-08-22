6f5822947150e6ede8a864dd54fc311774ad83c0
nalsae <nalsae0207@naver.com>
2023-08-22 23:15:07 +0900

    ♻️ Refactor : Signin 페이지의 접근성 개선 (#68)

--- src/app/signin/layout.tsx
+++ src/app/signin/layout.tsx
@@ -7,6 +7,7 @@ export default function SigninLayout({
 }) {
   return (
     <div className={twMerge('container-page', 'justify-center pb-14')}>
+      <h2 className="sr-only">로그인</h2>
       {children}
     </div>
   );
