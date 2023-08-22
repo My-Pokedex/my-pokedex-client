d25858d63839836f5379d50fa5741fb4dd09999d
nalsae <nalsae0207@naver.com>
2023-08-22 20:36:57 +0900

    ♻️ Refactor : SearchResult 페이지에 병렬 라우팅 적용 (#65)

--- src/app/searchresult/page.tsx
+++ src/app/searchresult/page.tsx
@@ -2,15 +2,10 @@ import { TypeFilters, Dropdown, PokemonCards } from '@/components';
 
 export default function SearchResult() {
   return (
-    <>
-      <section className="flex flex-col gap-4 mx-5 mt-8 mb-4">
-        <h2 className="sr-only">필터 선택</h2>
-        <TypeFilters usage="search" lang="ko" />
-        <Dropdown />
-      </section>
-      <main>
-        <PokemonCards lang="ko" />
-      </main>
-    </>
+    <section className="flex flex-col gap-4 mx-5 mt-8 mb-4">
+      <h2 className="sr-only">필터 선택</h2>
+      <TypeFilters usage="search" lang="ko" />
+      <Dropdown />
+    </section>
   );
 }
