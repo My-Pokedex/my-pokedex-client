6f1a3152a4d68ae886116a8c70924bcdd4a9c251
nalsae <nalsae0207@naver.com>
2023-08-22 23:14:52 +0900

    ♻️ Refactor : 전체 페이지의 h1 태그를 RootLayout으로 이동 (#68)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center h-full bg-[url('/assets/img/bg_home.jpg')] bg-cover bg-center bg-no-repeat">
       <NavBar className="absolute top-[3.75rem] right-0 z-50" />
       <main className="mx-4">
-        <h1 className="sr-only">My Pokedex</h1>
         <Logo location="main" className="mb-6" />
         <h2 className="sr-only">포켓몬 검색하기</h2>
         <SearchInput type="main" className="mb-[6.25rem]" />
